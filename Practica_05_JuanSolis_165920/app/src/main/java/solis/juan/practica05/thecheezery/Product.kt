454073c8717c07eb8bb486a4eff16dddef3349b3
jesp010 <juansolisosx@gmail.com>
2021-06-23 00:32:30 -0700

    Practica 05

---
+++ Practica_05_JuanSolis_165920/app/src/main/java/solis/juan/practica05/thecheezery/Product.kt
@@ -0,0 +1,6 @@
+package solis.juan.practica05.thecheezery
+
+data class Product(var name: String,
+                    var image: Int,
+                    var description: String,
+                    var price: Double)
